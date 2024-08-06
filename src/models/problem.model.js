8a933978bc2a5679178b4a67d5db98d6c839e387
girivardhan1919 <daggupati.girivardhan@gmail.com>
2024-08-06 23:20:07 +0530

    Problem service

---
+++ src/models/problem.model.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+const problemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Title cannot be empty']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description cannot be empty']
+    },
+    difficulty: {
+        type: String,
+        enum: ['easy', 'medium', 'hard'],
+        required: [true, 'Difficulty cannot be empty'],
+        default: 'easy'
+    },
+    testCases: [
+        {
+            input: {
+                type: String,
+                required: true
+            },
+            output: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    codeStubs: [
+        {
+            language: {
+                type: string,
+                enum: ["CPP", "JAVA", "PYTHON"],
+                required: true,
+            },
+            startSinppet: {
+                type: string,
+            },
+            endSnippet: {
+                type: string,
+            },
+            userSnippet: {
+                type: string,
+            }
+        }
+    ],
+    editorial: {
+        type: String
+    }
+});
+
+const Problem = mongoose.model('Problem', problemSchema);
+
+module.exports = Problem;
+
+/**
+ * 
+ * [{input: '5', output: '10'}, {input: '2', output: '20'}]
+ * 
+ * 
+ * 5
+ * 2 3 4 5 6
+ * 8
+ * 
+ * 1 3
+ * 
+ */
